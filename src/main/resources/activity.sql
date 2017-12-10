9d5d3ec455ad457df1d395fb5e7db8b5cb93986c
PersistXL <pointinglxl@gmail.com>
2017-12-10 12:43:38 +0800

    add sql

--- src/main/resources/activity.sql
+++ src/main/resources/activity.sql
@@ -15,9 +15,7 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
---
--- Table structure for table `evaluate`
---
+
 
 DROP TABLE IF EXISTS `evaluate`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
